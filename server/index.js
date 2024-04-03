b9b060b5575e7b2ed078824b87739c91fc361dea
LogeshLVT <logesh262004@gmail.com>
2024-04-03 15:43:17 +0530

    Intial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const mongoose  =require('mongoose');
+const dotenv =require('dotenv').config();
+
+const app =express();
+//use express.json() to get data into json format 
+app.use(express.json());
+//port 
+const PORT =process.env.PORT||5500;
+
+//connect to server 
+app.listen(PORT, ()=> console.log("server connected"));
+
